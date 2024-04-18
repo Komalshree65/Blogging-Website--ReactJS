8bdf8365d452f8a21177b81b75d2893d2b701243
Komalshree65 <125942681+Komalshree65@users.noreply.github.com>
2024-04-18 17:29:33 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,13 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm"> Exploring Journeys </span>
+        <span className="headerTitleLg">ALLURE</span>
+      </div>
+      <img className="headerImg" src="https://wallup.net/wp-content/uploads/2018/03/23/561146-colorful-nature-sunlight-plants-flowers.jpg" alt="" />
+    </div>
+  );
+}
